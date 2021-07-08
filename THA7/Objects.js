1cf01dabf107e1d1a579266d2ff4b4930f27884d
Tejashree Thosar <55942093+tejashreethosar@users.noreply.github.com>
2021-07-08 16:49:45 +0530

    Update Objects.js

--- THA7/Objects.js
+++ THA7/Objects.js
@@ -28,16 +28,31 @@ console.log(x);
 
 /* 4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; */
 
-var library = [ 
-	{ author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
- 	{ author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, 
-	{ author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }
+var library = [{
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+  },
+  {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
+  },
+  {
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+  }
 ];
 
-for (var i = 0; i < library.length; i++) 
-{
-    var book = 'The book is ' + "'" + library[i].title + "'" + ' by ' + library[i].author + ' with reading status ' + library[i].readingStatus + '.';
+for (var i = 0; i < library.length; i++) {
+  if (library[i].readingStatus == true) {
+    var book = 'The book is ' + "'" + library[i].title + "'" + ' by ' + library[i].author + ' has reading status ' + 'completed.';
+    console.log(book); 
+  } else {
+    var book = 'The book is ' + "'" + library[i].title + "'" + ' by ' + library[i].author + ' has reading status ' + 'incomplete.';
     console.log(book);
+  }
 }
 
 // --------------------------------------------------------------------
